3796eccee3f832bd15bf05384983325e99ca38fb
LDdidier <didier.dogo@epitech.eu>
2024-12-19 12:26:00 +0000

    depot avacement dans inscription

---
+++ popup/models/database.php
@@ -0,0 +1,19 @@
+<?php
+
+function ConnectDatabase(){
+    try{
+       $connexion = new PDO("mysql:host=localhost;dbname=my_shop;port=3306","root","Christ1609");
+       $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }catch(Exception $e){
+        echo "Erreur :". $e->getMessage()."\n";
+        // exit();
+
+    }
+    return $connexion;
+}
+
+//Récuparation des champs
+$nomUtilisateurDB = $_POST["username"];
+$emailDB = $_POST["email"];
+$passwordDB = $_POST["password"];
+$confirmePasswordDB = $_POST["confirm-password"];
